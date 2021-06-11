2d64a70184cc6cca1e36c9b347bb5beccbbddce5
Aakash <saakash1012@gmail.com>
2021-06-11 17:12:16 +0530

    completed

--- Aakash_Exercise01/Program.cs
+++ Aakash_Exercise01/Program.cs
@@ -16,6 +16,14 @@ namespace Aakash_Exercise01
         {
             Func<string, string, string, string> minStringFunc = MinimumV;
             Console.WriteLine($"Using a built-in delegate template- Minimum call : {minStringFunc("Hello world", "Hello world there", "Hello world threeeeee")}");
+
+            Action<int, int, int> AvgGrade = Average;
+            AvgGrade(5,10,15);
+        }
+
+        private static void Average(int arg1, int arg2, int arg3)
+        {
+            Console.WriteLine("Average of three grades: "+(arg1+arg2+arg3)/3);
         }
 
         private static string MinimumV(string string1, string string2, string string3)
