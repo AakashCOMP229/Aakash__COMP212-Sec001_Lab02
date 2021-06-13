764ce6a4ec5a5a56adf78893c58ac4ba2b02d0e0
Aakash <saakash1012@gmail.com>
2021-06-13 18:18:19 +0530

    final commit

--- Aakash_Exercise01/Program.cs
+++ Aakash_Exercise01/Program.cs
@@ -1,37 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Aakash_Exercise01
 {
     class Program
     {
-        // declaring a delegate
-        // delegate for a function which returns the smallest of three string values.
-        //
-               public delegate string Minimum(string string1, string string2, string string3);
         static void Main(string[] args)
         {
-            Func<string, string, string, string> minStringFunc = MinimumV;
-            Console.WriteLine($"Using a built-in delegate template- Minimum call : {minStringFunc("Hello world", "Hello world there", "Hello world threeeeee")}");
+            //declaring built-in delegate- Func<> with three input arugment and one string return type
+            //calling minimum method via Func<string, string, string, string> minStringFunc
+            Func<string, string, string, string> minStringFunc = Minimum;
+            Console.WriteLine($"Using a Func<>, a built-in delegate template:\nSmallest of three string values: {minStringFunc("Hello world", "Hello world there", "Two")}");
 
+            //declaring built-in delegate- Action<> with three input arugment and no return type            
             Action<int, int, int> AvgGrade = Average;
+            //calling Average method via Action<int, int, int> AvgGrade
             AvgGrade(5,10,15);
         }
 
+        //Average method
+        //to display average of three grades
         private static void Average(int arg1, int arg2, int arg3)
         {
-            Console.WriteLine("Average of three grades: "+(arg1+arg2+arg3)/3);
+            Console.WriteLine("\nUsing a Action, a built-in delegate template:\nAverage of three grades: " + (arg1+arg2+arg3)/3);
         }
 
-        private static string MinimumV(string string1, string string2, string string3)
+        //Minimum method
+        //to return the smallest of three string values. 
+        private static string Minimum(string string1, string string2, string string3)
         {
-            string miniValue, minValue = default;
+            string minimumStringValue, minValue;
             minValue = string1.Length < string2.Length ? string1 : string2;
-            miniValue = minValue.Length < string3.Length ? minValue : string3;
-            return miniValue;
+            minimumStringValue = minValue.Length < string3.Length ? minValue : string3;
+            return minimumStringValue;
         }
     }
 }
---
+++ Aakash_Exercise02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Aakash_Exercise02
+{
+    class Program
+    {
+        // declaring a delegate
+        // delegate for a function that receives an double and returns a bool
+        public delegate bool GradesPredicate(double numbers);
+        static void Main(string[] args)
+        {
+            double[] gradesArray = { 40.25, 52.35, 34.50, 64.30, 75.15, 86.23, 77.44, 58.33, 39.24, 90.95 };
+
+            GradesPredicate gradesPredicate = numbersOver50 => numbersOver50 >= 50;
+            GradesFilter(gradesArray, gradesPredicate);
+        }
+
+        //displays only those grades values which are greater than or equal to 50
+        public static void GradesFilter(double[] grades, GradesPredicate gradesPredicate)
+        {
+            Console.WriteLine("Grades over 50 are as follows: ");
+            // iterate over each element in the array
+            foreach (var item in grades)
+            {
+                // if the element satisfies the predicate
+                if (gradesPredicate(item))
+                {
+                    Console.WriteLine(item);
+                }
+            }            
+        }
+    }
+}
